aeae840e18e21964cf17152c56a652685ba2ac2d
Olha <fidelevna@mail.ru>
2016-04-21 23:06:50 +0300

    clear and dot

--- application.js
+++ application.js
@@ -1,13 +1,36 @@
 var calculator = {
   screen: document.getElementById('screen'),
   digits: document.getElementsByClassName('digit'),
+  clear: document.getElementById('clear'),
+  dot: document.getElementById('dot'),
+  dotClicked: false,
+
   start: function() {
     for(var i = 0; i < calculator.digits.length; i++) {
        calculator.digits[i].onclick = function(event) {
          var chislo = event.target.innerHTML;
-         calculator.screen.value = calculator.screen.value+chislo;
+         calculator.screen.innerHTML = calculator.screen.innerHTML+chislo;
        }
      }
-  }
+     calculator.clear.onclick = function() {
+       calculator.screen.innerHTML = '';
+       calculator.dotClicked = false;
+     }
+     calculator.dot.onclick = function(event) {
+       if (!calculator.dotClicked) {
+         var dot = event.target.innerHTML;
+         calculator.screen.innerHTML = calculator.screen.innerHTML+dot;
+         calculator.dotClicked = true;     
+       }
+     }
+   }
 };
 calculator.start();
+
+//  if (calculator.dotClicked) {
+ //
+//  } else{
+//    var dot = event.target.innerHTML;
+//    calculator.screen.innerHTML = calculator.screen.innerHTML+dot;
+//    calculator.dotClicked = true;
+//  }
