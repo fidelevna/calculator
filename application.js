793ceb987f7beb0ef22b49f98faf3299d7ea8a6e
Olha <fidelevna@mail.ru>
2016-04-26 20:58:00 +0300

    plus and dot

--- application.js
+++ application.js
@@ -3,13 +3,25 @@ var calculator = {
   digits: document.getElementsByClassName('digit'),
   clear: document.getElementById('clear'),
   dot: document.getElementById('dot'),
-  dotClicked: false,
+  plus: document.getElementById('plus'),
+  equal: document.getElementById('equal'),
 
+  dotClicked: false,
+  shouldClearScreen: false,
+
+  addition: function(argument1, argument2) {
+    return argument1+argument2;
+  },
   start: function() {
     for(var i = 0; i < calculator.digits.length; i++) {
        calculator.digits[i].onclick = function(event) {
          var chislo = event.target.innerHTML;
-         calculator.screen.innerHTML = calculator.screen.innerHTML+chislo;
+         if (calculator.shouldClearScreen) {
+           calculator.screen.innerHTML = chislo;
+           calculator.shouldClearScreen = false;
+         } else {
+           calculator.screen.innerHTML = calculator.screen.innerHTML+chislo;
+         }
        }
      }
      calculator.clear.onclick = function() {
@@ -19,9 +31,26 @@ var calculator = {
      calculator.dot.onclick = function(event) {
        if (!calculator.dotClicked) {
          var dot = event.target.innerHTML;
-         calculator.screen.innerHTML = calculator.screen.innerHTML+dot;
-         calculator.dotClicked = true;     
+         if (calculator.shouldClearScreen) {
+          calculator.screen.innerHTML = "0" + dot;
+          calculator.shouldClearScreen = false;
+        } else {
+          calculator.screen.innerHTML = calculator.screen.innerHTML+dot;
+        }
+         calculator.dotClicked = true;
        }
+     }
+     calculator.plus.onclick = function() {
+       calculator.argument1 = parseFloat(calculator.screen.innerHTML);
+       calculator.shouldClearScreen = true;
+       calculator.operation = "addition";
+       calculator.dotClicked = false;
+     }
+     calculator.equal.onclick = function() {
+       calculator.argument2 = parseFloat(calculator.screen.innerHTML);
+       var result = calculator[calculator.operation](calculator.argument1, calculator.argument2);
+       calculator.screen.innerHTML = result;
+       calculator.dotClicked = false;
      }
    }
 };
